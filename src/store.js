df1f82fd99251b4eedaf7c3847a40025f05115fb
Ahla Fayis <139907384+FathimaAhla@users.noreply.github.com>
2024-01-03 21:36:50 +0530

    fix: correct the spelling mistake

--- src/store.js
+++ src/store.js
@@ -5,6 +5,6 @@ export const store = configureStore({
     devTools:true,
     // The reducer is used in conjunction with useState to handle functions within Redux
     reducer: {
-        customer: ""
+        customers: customerReducer
     }
 })
