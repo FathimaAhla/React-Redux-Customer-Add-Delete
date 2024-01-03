05f2da2c43a0982996bd92ba5c17d055e22ac575
Ahla Fayis <139907384+FathimaAhla@users.noreply.github.com>
2024-01-03 19:59:44 +0530

    feat: create Reducers function customerSlice.js

--- src/slices/customerSlices.js
+++ src/slices/customerSlices.js
@@ -5,5 +5,12 @@ const initialState = [];
 const customerSlice = createSlice({
     name: 'customer',
     initialState: initialState, // Key value
+    // If there are any changes in the customer data on the store page, it triggers the corresponding functions in this reducer.
+    reducers: {
+        // make input changes
+        addCustomer(state, action) {
+            state.push(action.payload)
+        }
+    }
 
 })
