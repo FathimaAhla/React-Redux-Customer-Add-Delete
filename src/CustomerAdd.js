db233f25b2a8444c4bff6ea2827d9ba02e965ad6
Ahla Fayis <139907384+FathimaAhla@users.noreply.github.com>
2024-01-03 21:35:35 +0530

    feat: add dispatch into customerAdd

--- src/CustomerAdd.js
+++ src/CustomerAdd.js
@@ -1,14 +1,18 @@
 import React, { useState } from "react";
 import CustomerView from "./CustomerView";
+import { addCustomer as addCustomerAction } from "./slices/customerSlices";
+import { useDispatch } from "react-redux";
 
 export default function CustomerAdd() {
 
     const [input, setInput] = useState("");
-    const [customers, setCustomers] = useState([]);
+    // const [customers, setCustomers] = useState([]);
+    const dispatch = useDispatch();
 
     function addCustomer() {
         if(input) {
-            setCustomers((previousState) => [...previousState, input])
+            // setCustomers((previousState) => [...previousState, input])
+            dispatch(addCustomerAction(input));
             setInput("");
         }
     }
@@ -19,6 +23,5 @@ export default function CustomerAdd() {
                 <input type="text" value={input} onChange={(e) => setInput(e.target.value)}/>
                 <button onClick={addCustomer}>Add</button>
             </div>
-            <CustomerView customers={customers}/>
         </>
 }
