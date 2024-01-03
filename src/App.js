a5108325ca77667e24613ad6cf7b4fb0adacb62c
Ahla Fayis <139907384+FathimaAhla@users.noreply.github.com>
2024-01-03 21:28:54 +0530

    feat: add customerView component into app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Provider } from 'react-redux';
 import './App.css';
 import CustomerAdd from './CustomerAdd';
 import { store } from './store';
+import CustomerView from './CustomerView';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className="App">
         <h1>React Redux Customer Example</h1>
         <CustomerAdd />
+        <CustomerView />
       </div>
     </Provider>
   );
