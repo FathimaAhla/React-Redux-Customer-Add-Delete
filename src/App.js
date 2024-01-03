beb1efe8d417a440cf26bdc83fda8280df9a7847
Ahla Fayis <139907384+FathimaAhla@users.noreply.github.com>
2024-01-03 20:10:07 +0530

    feat: sharing redux store using provider App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import { Provider } from 'react-redux';
 import './App.css';
 import CustomerAdd from './CustomerAdd';
+import { store } from './store';
 
 function App() {
   return (
-    <div className="App">
-      <h1>React Redux Customer Example</h1>
-      <CustomerAdd />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <h1>React Redux Customer Example</h1>
+        <CustomerAdd />
+      </div>
+    </Provider>
   );
 }
 
